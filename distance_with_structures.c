df191e1ad3d6e8d195ddf6c7f09cba7869eff10f
ENG19CS0009-ABHIJEETKUMAR <76103326+ENG19CS0009-ABHIJEETKUMAR@users.noreply.github.com>
2021-03-27 16:09:01 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,38 +1,45 @@
 //WAP to find the distance between two points using structures and 4 functions.
 #include <stdio.h>
-#include <math.h>
+#include<math.h>
+
 struct point
 {
-    float x;
-    float y;
+    float x,y;
 };
+
 typedef struct point Point1;
+
 Point1 input()
 {
     Point1 p;
-    printf("Enter abscissa ");
-    scanf("%f",&(p.x));
-    printf("Enter ordinate ");
+    printf("Enter abscissa : ");
+    scanf("%f",&p.x);
+    printf("Enter ordinate : ");
     scanf("%f",&p.y);
     return p;
 }
-float compute(Point1 p1,Point1 p2)
-{
-    float distance;
-    distance=sqrt(pow((p1.x-p2.x),2)+pow((p1.y-p2.y),2));
-    return distance;
-}
-void output(Point1 p1, Point1 p2, float dist)
-{
-    printf("The distance between %f,%f and %f,%f is %f",p1.x,p1,y,p2.x,p2,y,dist);
-}
-int main()
+
+float distance(Point1 p1,Point1 p2)
 {
     float dist;
+    dist = sqrt(pow(((p2.x)-(p1.x)),2)+pow(((p2.y)-(p1.y)),2));
+    return dist;
+}
+
+void output(Point1 p1,Point1 p2,float distance)
+{
+    printf("The distance between (%.3f,%.3f) and (%.3f,%.3f) is %.3f .\n",(p1.x),(p1.y),(p2.x),(p2.y),distance);
+       
+}
+
+int main()
+{
     Point1 p1,p2;
+    float d;
     p1=input();
     p2=input();
-    dist=compute(p1,p2);
-    output(p1,p2,dist);
+    d=distance(p1,p2);
+    output(p1,p2,d);
+
     return 0;
 }
